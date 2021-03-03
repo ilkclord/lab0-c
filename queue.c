90813a56d2d90d182a6c43743404c7044e8eb413
ilkclord <smpss96243@gmail.com>
2021-03-03 23:16:18 +0800

    Finally ! there's only sort() and free() left I leraned a lot by debugging orz..

--- queue.c
+++ queue.c
@@ -13,7 +13,10 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
+    if (q == NULL)
+        return q;
     q->head = NULL;
+    q->tail = NULL;
     return q;
 }
 
@@ -24,23 +27,34 @@ void q_free(queue_t *q)
     /* Free queue structure */
     free(q);
 }
-
-/*
- * Attempt to insert element at head of queue.
- * Return true if successful.
- * Return false if q is NULL or could not allocate space.
- * Argument s points to the string to be stored.
- * The function must explicitly allocate space and copy the string into it.
- */
+/* * Attempt to insert element at head of queue.  * Return true if successful.
+ * * Return false if q is NULL or could not allocate space.  * Argument s points
+ * to the string to be stored.  * The function must explicitly allocate space
+ * and copy the string into it.  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
     /* TODO: What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
+    if (q == NULL)
+        return false;
+    lsit_ele_t *newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL) {
+        free(newh);
+        return false;
+    }
+    char *tmp = malloc(strlen(s) + 1);
+    if (tmp == NULL) {
+        free(tmp);
+        free(newh);
+        return false;
+    }
+    strncpy(tmp, s, strlen(s) + 1);
+    newh->value = tmp;
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     newh->next = q->head;
     q->head = newh;
+    if (q->tail == NULL)
+        q->tail = newh->next;
     return true;
 }
 
@@ -54,9 +68,27 @@ bool q_insert_head(queue_t *q, char *s)
 bool q_insert_tail(queue_t *q, char *s)
 {
     /* TODO: You need to write the complete code for this function */
+    if (q == NULL)
+        return false;
+    lsit_ele_t *newt = malloc(sizeof(list_ele_t));
+    if (newt == NULL) {
+        free(newt);
+        return false;
+    }
+    char *tmp = malloc(strlen(s) + 1);
+    if (tmp == NULL) {
+        free(tmp);
+        free(newt);
+        return false;
+    }
+    strncpy(tmp, s, strlen(s) + 1);
+    newt->next = NULL;
+    newt->value = tmp;
+    q->tail->next = newt;
+    q->tail = newt;
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    return true;
 }
 
 /*
@@ -71,6 +103,14 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* TODO: You need to fix up this code. */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (q == NULL || q->head == NULL)
+        return false;
+    sp = malloc(sizeof(char) * (bufsize + 1));
+    for (int i = 0; i < bufsize; i++)
+        sp[i] = q->head->value[i];
+    sp[bufsize] = '\0';
+    free(q->head->value);
+    free(q->head->next);
     q->head = q->head->next;
     return true;
 }
@@ -83,8 +123,8 @@ int q_size(queue_t *q)
 {
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    /* TODO: Remove the above comment when you are about implement. */
+    return q->size;
 }
 
 /*
@@ -97,16 +137,29 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     /* TODO: You need to write the code for this function */
+    if (q != NULL && q->head != NULL) {
+        list_ele_t *tmp = q->head->next;
+        lsit_ele_t *tmp2;
+        q->head->next = NULL;
+        while (tmp != NULL) {
+            tmp2 = tmp->next;
+            tmp->next = q->head;
+            q->head = tmp;
+            tmp = tmp2;
+        }
+        q->head = tmp;
+    }
     /* TODO: Remove the above comment when you are about to implement. */
 }
 
 /*
- * Sort elements of queue in ascending order
+ * sort elements of queue in ascending order
  * No effect if q is NULL or empty. In addition, if q has only one
  * element, do nothing.
  */
 void q_sort(queue_t *q)
 {
     /* TODO: You need to write the code for this function */
+
     /* TODO: Remove the above comment when you are about to implement. */
 }
