646d283785dbd9ff2f0e51902a66efa6f07b5944
ilkclord <smpss96243@gmail.com>
2021-03-06 00:58:30 +0800

    The queue.c is done and the next job is console !

--- queue.c
+++ queue.c
@@ -24,6 +24,12 @@ queue_t *q_new()
 void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
+    while (q->head != NULL) {
+        list_ele_t *tmp = q->head->next;
+        free(q->value);
+        free(q->next);
+        q->head = tmp;
+    }
     /* Free queue structure */
     free(q);
 }
@@ -36,7 +42,7 @@ bool q_insert_head(queue_t *q, char *s)
     /* TODO: What should you do if the q is NULL? */
     if (q == NULL)
         return false;
-    lsit_ele_t *newh = malloc(sizeof(list_ele_t));
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
     if (newh == NULL) {
         free(newh);
         return false;
@@ -70,7 +76,7 @@ bool q_insert_tail(queue_t *q, char *s)
     /* TODO: You need to write the complete code for this function */
     if (q == NULL)
         return false;
-    lsit_ele_t *newt = malloc(sizeof(list_ele_t));
+    list_ele_t *newt = malloc(sizeof(list_ele_t));
     if (newt == NULL) {
         free(newt);
         return false;
@@ -105,13 +111,14 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
     /* TODO: Remove the above comment when you are about to implement. */
     if (q == NULL || q->head == NULL)
         return false;
-    sp = malloc(sizeof(char) * (bufsize + 1));
-    for (int i = 0; i < bufsize; i++)
-        sp[i] = q->head->value[i];
-    sp[bufsize] = '\0';
+    if (sp == NULL)
+        return false;
+    strncpy(sp, q->head->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
+    lsit_ele_t tmp = q->head->next;
     free(q->head->value);
     free(q->head->next);
-    q->head = q->head->next;
+    q->head = tmp;
     return true;
 }
 
@@ -139,7 +146,7 @@ void q_reverse(queue_t *q)
     /* TODO: You need to write the code for this function */
     if (q != NULL && q->head != NULL) {
         list_ele_t *tmp = q->head->next;
-        lsit_ele_t *tmp2;
+        list_ele_t *tmp2;
         q->head->next = NULL;
         while (tmp != NULL) {
             tmp2 = tmp->next;
@@ -157,9 +164,60 @@ void q_reverse(queue_t *q)
  * No effect if q is NULL or empty. In addition, if q has only one
  * element, do nothing.
  */
-void q_sort(queue_t *q)
+list_ele_t *merge(list_ele_t *q1, list_ele_t *q2)
 {
+    list_ele_t *leads = NULL, *root;
+    if (strcmp(q1->value, q2->value) < 0) {
+        leads = q1;
+        q1 = q1->next;
+    } else {
+        leads = q2;
+        q2 = q2->next;
+    }
+    root = leads;
+    while (q1 != NULL && q2 != NULL) {
+        if (strcmp(q1->value, q2->value) < 0) {
+            leads->next = q1;
+            q1 = q1->next;
+        } else {
+            leads->next = q2;
+            q2 = q2->next;
+        }
+        leads = leads->next;
+    }
+    if (q1 == NULL)
+        leads->next = q2;
+    else
+        leads->next = q1;
+    return root;
+}
+list_ele_t *merge_find(int a, list_ele_t *root)
+{
+    while (a > 1) {
+        root = root->next;
+        a = a - 1;
+    }
+    return root;
+}
+lsit_ele_t *merge_split(list_ele_t *root, int size)
+{
+    if (size / 2 > 0) {
+        list_ele_t *tmp = merge_find(size / 2, root);
+        lsit_ele_t *q2 = tmp->next;
+        tmp->next = NULL;
+        root = merge_split(root, size / 2);
+        q2 = merge_split(q2, size - size / 2);
+        return merge(root, q2);
+    } else {
+        root->next = NULL;
+        return root;
+    }
+}
+void q_sort(queue_t *q)
+{  // 9 8 10 7
     /* TODO: You need to write the code for this function */
-
+    if (q == NULL || q->head == NULL || q->head->next == NULL)
+        return;
+    q->head = merge_split(q->head, q->size);
     /* TODO: Remove the above comment when you are about to implement. */
 }
